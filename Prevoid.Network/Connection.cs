b9b223d8f4a45d434a5bef0c33b9c2f04391817c
Illia Aprol <asdf21072107@gmail.com>
2021-12-11 22:52:03 +0300

    Add basic Connection logic

--- Prevoid.Network/Connection.cs
+++ Prevoid.Network/Connection.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Prevoid.Network
 {
     public class Connection
     {
+        public Socket Remote { get; private set; }
+        private static readonly Encoding _Encoding = Encoding.UTF8;
+        private const int _Port = 11000;
+        private const int _MessageMaxSize = 2048;
+
         /// <summary>
         /// Create connection as a server.
         /// </summary>
@@ -20,17 +27,20 @@ namespace Prevoid.Network
         /// <param name="ip">Server's IP</param>
         public Connection(string ip)
         {
-            throw new NotImplementedException();
+            Remote = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            Remote.Connect(ip, _Port);
         }
 
         public async Task SendText(string text)
         {
-            throw new NotImplementedException();
+            await Task.Run(() => Remote.Send(_Encoding.GetBytes(text)));
         }
 
         public string RecieveText()
         {
-            throw new NotImplementedException();
+            byte[] buffer = new byte[_MessageMaxSize];
+            Remote.Receive(buffer);
+            return _Encoding.GetString(buffer);
         }
     }
 }
